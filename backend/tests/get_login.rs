4b2841e3645f0fd442b65f9a94647f120e438c1f
shtsoft <git@shtsoft.eu>
2024-08-08 14:43:32 +0200

    backend: Add tests for handlers serving frontend

---
+++ backend/tests/get_login.rs
@@ -0,0 +1,23 @@
+mod helpers;
+
+use helpers::make_clients;
+use helpers::ADDR;
+
+#[tokio::test]
+async fn test_get_login() {
+    let (client, _) = make_clients();
+
+    let response_ok_1 = client
+        .get(format!("https://{}/", ADDR))
+        .send()
+        .await
+        .expect("Failed to send request");
+    assert!(response_ok_1.status().is_success());
+
+    let response_ok_2 = client
+        .get(format!("https://{}/index.html", ADDR))
+        .send()
+        .await
+        .expect("Failed to send request");
+    assert!(response_ok_2.status().is_success());
+}
